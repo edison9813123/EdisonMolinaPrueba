0b4729535e72ad1d4c4bfbc37772acd22a718414
edison9813123 <61266806+edison9813123@users.noreply.github.com>
2021-02-04 19:03:25 -0500

    Update Program.cs

--- EdisonMolinaPrueba/Program.cs
+++ EdisonMolinaPrueba/Program.cs
@@ -15,89 +15,92 @@ namespace EdisonMolinaPrueba
             Thread piso1 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 1:");
             piso1.Start();
+            Piso1.Join();
+            
             Thread piso2 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 2:");
             piso2.Start();
+             Piso2.Join();
+            
             Thread piso3 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 3:");
             piso3.Start();
+            Piso3.Join();
+            
             Thread piso4 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 4:");
             piso4.Start();
+             Piso4.Join();
+            
             Thread piso5 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 5:");
             piso5.Start();
+            Piso5.Join();
+            
             Thread piso6 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 6:");
             piso6.Start();
+             Piso6.Join();
+            
             Thread piso7 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 7:");
             piso7.Start();
+            Piso7.Join();
+            
             Thread piso8 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 8:");
             piso8.Start();
+            Piso8.Join();
+            
+            
+            
             Thread piso9 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 9:");
             piso9.Start();
+             Piso9.Join();
+            
             Thread piso10 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 10:");
             piso10.Start();
+            
+            Piso10.Join();
+            
             Thread piso11 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 11:");
             piso11.Start();
+            
+            Piso11.Join();
+            
             Thread piso12 = new Thread(NumeroDePersonas);
             Console.WriteLine("Piso 12:");
             piso12.Start();
+               Piso12.Join();
 
-
-            Piso1.Join();
-            Piso2.Join();
-            Piso3.Join();
-            Piso4.Join();
-            Piso5.Join();
-            Piso6.Join();
-            Piso7.Join();
-            Piso8.Join();
-            Piso9.Join();
-            Piso10.Join();
-            Piso11.Join();
-            Piso12.Join();
-
+        
         }
         public static void NumeroDePersonas()
         {
             var Apartamento = 0;
 
-            for (int i = 0; i <= 11; i++)
+            for (int i = 0; i <= 12; i++)
             {
-                var guid = Guid.NewGuid();
-                var justNumbers = new String(guid.ToString().Where(Char.IsDigit).ToArray());
-                var randomDepartamento = new Random();
-                if (Apartamento <= 7)
+                var guia = Guia.NewGuid();
+                var nummeros = new String(guid.ToString().Where(Char.IsDigit).ToArray());
+                var randomApartamento = new Random();
+                if (Apartamento <= 8)
                 {
                     Apartamento ++;
                 }
-
-
                 var random = new Random();
-                var value = random.Next(0, 6);
+                var value = random.Next(0, 12);
 
-                Console.WriteLine($"Piso {i} - departamentos {Apartamento} - cantidad de personas {value}");
+                Console.WriteLine($"Piso {i} - Apartamentos {Apartamento} - cantidad de personas dentro del edificio {value}");
             }
         }
     }
 }
 
 
-
-
-        }
-    }
-}
-
-            
-           
-            
             
 
             
